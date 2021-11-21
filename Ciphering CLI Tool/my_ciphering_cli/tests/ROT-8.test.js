e6bb7586275de0ad4227c1654e72868de9364cb0
excluz1v <bmd4work@gmail.com>
2021-11-21 15:59:14 +0600

    feat: add test to rot8 cipher

---
+++ Ciphering CLI Tool/my_ciphering_cli/tests/ROT-8.test.js
@@ -0,0 +1,32 @@
+const { rot8Cipher } = require('../ROT-8')
+
+describe('return correct letters', () => {
+    const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const resultR1 = "IJKLMNOPQRSTUVWXYZABCDEFGH"
+    const resultR0 = "STUVWXYZABCDEFGHIJKLMNOPQR"
+    test("pass for upperCase letters", () => {
+        expect(rot8Cipher(uppercaseLetters, 'R1')).toBe(resultR1)
+        expect(rot8Cipher(uppercaseLetters, 'R0')).toBe(resultR0)
+    }
+    )
+
+    const lowerCaseLetters = uppercaseLetters.toLowerCase()
+    const resultR1Lower = resultR1.toLowerCase()
+    const resultR0Lower = resultR0.toLowerCase()
+
+    test("pass for upperCase letters", () => {
+        expect(rot8Cipher(lowerCaseLetters, 'R1')).toBe(resultR1Lower)
+
+        expect(rot8Cipher(lowerCaseLetters, 'R0')).toBe(resultR0Lower)
+    }
+    )
+
+    const notLetters = '12345!@#$%^&*()-+'
+
+    test('pass for non-letters', () => {
+        expect(rot8Cipher(notLetters, 'R1')).toBe(notLetters)
+        expect(rot8Cipher(notLetters, 'R0')).toBe(notLetters)
+    })
+
+
+})
